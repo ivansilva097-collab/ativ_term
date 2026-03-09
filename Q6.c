ca2bf29fe36ba3ca07429df1257ea7d353599afa
ivansilva097-collab <ivan.silva.097@ufrn.edu.br>
2026-03-09 15:09:08 -0300

    Update Q6.c

--- Q6.c
+++ Q6.c
@@ -4,11 +4,10 @@
 
 int main(){
     int x;
-    int e = 0;
     int div = 0;
     scanf("%d", &x);
     if (x <= 1){
-        e = 0;
+        div = 0;
     } else {
         for (int i = 1; i <= x; i++){
             if (x % i == 0){
@@ -17,9 +16,6 @@ int main(){
         }
     }
     if (div == 2){
-        e = 1;
-    }
-    if (e == 1){
         printf("%d e primo", x);
     } else {
         printf("%d nao e primo", x);
